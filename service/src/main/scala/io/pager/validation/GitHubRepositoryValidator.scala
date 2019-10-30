88eb8646986945b3b6af8ea37fa75577a4f0c284
Pavels Sisojevs <p.sisoyev@gmail.com>
2019-10-30 16:22:52 +0100

    fix saved url

--- service/src/main/scala/io/pager/validation/GitHubRepositoryValidator.scala
+++ service/src/main/scala/io/pager/validation/GitHubRepositoryValidator.scala
@@ -3,22 +3,26 @@ package io.pager.validation
 import io.pager.PagerError.NotFound
 import io.pager.api.http._
 import io.pager.logger._
-import io.pager.{ LoggingEnv, PagerError, Subscription, ValidatorEnv }
+import io.pager.{LoggingEnv, PagerError, Subscription, ValidatorEnv}
 import zio.ZIO
 
 trait GitHubRepositoryValidator extends RepositoryValidator.Service[ValidatorEnv] {
   def validate(name: String): ZIO[ValidatorEnv, PagerError, Subscription.RepositoryUrl] = {
-    def failure(e: Throwable): ZIO[LoggingEnv, PagerError, Nothing] =
+    def failure: ZIO[LoggingEnv, PagerError, Nothing] =
       info(s"Failed to find repository $name") *> ZIO.fail(NotFound(name))
 
-    def success(s: String): ZIO[LoggingEnv, PagerError, Subscription.RepositoryUrl] =
-      info(s"Validated repository $name") *> ZIO.succeed(Subscription.RepositoryUrl(name))
+    def success(url: String): ZIO[LoggingEnv, PagerError, Subscription.RepositoryUrl] =
+      info(s"Validated repository $name") *> ZIO.succeed(Subscription.RepositoryUrl(url))
 
     //      TODO remove last /
     val url =
       if (name.startsWith("https://github.com/") || name.startsWith("http://github.com/")) name
       else s"https://github.com/$name"
 
-    get(s"$url/releases").foldM(failure, success)
+    get(s"$url/releases")
+      .foldM(
+        _ => failure,
+        _ => success(url)
+      )
   }
 }
