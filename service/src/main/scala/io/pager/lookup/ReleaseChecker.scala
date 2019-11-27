438bcd2f36ed67ddee3aee3dbf24e8f9d970b96b
Pavels Sisojevs <p.sisoyev@gmail.com>
2019-11-27 16:46:40 +0100

    implement missing logic: compare and update repository versions

--- service/src/main/scala/io/pager/lookup/ReleaseChecker.scala
+++ service/src/main/scala/io/pager/lookup/ReleaseChecker.scala
@@ -1,11 +1,14 @@
 package io.pager.lookup
 
-import io.pager.api.github.GitHubClient
-import io.pager.api.telegram.TelegramClient
+import io.pager.PagerError
+import io.pager.client.github.GitHubClient
+import io.pager.client.telegram.TelegramClient
 import io.pager.logging._
 import io.pager.subscription.RepositoryStatus.Version
-import io.pager.subscription.{ RepositoryName, RepositoryStatus, Subscription }
-import zio.{ Task, ZIO }
+import io.pager.subscription.{ RepositoryName, RepositoryStatus, SubscriptionLogic }
+import zio.{ IO, Task, UIO, ZIO }
+
+import scala.util.Try
 
 trait ReleaseChecker {
   val releaseChecker: ReleaseChecker.Service
@@ -20,28 +23,56 @@ object ReleaseChecker {
     def logger: Logger.Service
     def gitHubClient: GitHubClient.Service
     def telegramClient: TelegramClient.Service
-    def subscription: Subscription.Service
+    def subscription: SubscriptionLogic.Service
 
     override val releaseChecker: Service = new Service {
 
       override def scheduleRefresh: Task[Unit] =
         for {
-          _     <- logger.info("Getting latest repository versions")
-          repos <- subscription.listRepositories
-          _     <- broadcastNewVersions(repos)
-          _     <- logger.info("Finished repository refresh")
+          _               <- logger.info("Getting latest repository versions")
+          repos           <- subscription.listRepositories
+          latestVersions  <- latestRepositoryVersions(repos.keySet)
+          updatedVersions = newVersions(repos, latestVersions)
+          statuses        <- repositoryStatuses(updatedVersions)
+          _               <- subscription.updateVersions(updatedVersions)
+          _               <- broadcastUpdates(statuses)
+          _               <- logger.info("Finished repository refresh")
         } yield ()
     }
 
-    private def broadcastNewVersions(repos: Map[RepositoryName, RepositoryStatus]): Task[Unit] =
+    private def repositoryStatuses(updatedVersions: Map[RepositoryName, Version]): UIO[Map[RepositoryName, RepositoryStatus]] =
       ZIO
-        .traverse(repos) {
-          case (name, status) =>
-            gitHubClient.releases(name).flatMap { releases =>
-              val latest = releases.maxBy(_.published_at)
-              telegramClient.broadcastNewVersion(name, status.copy(version = Some(Version(latest.name))))
-            }
+        .foreach(updatedVersions) {
+          case (name, version) =>
+            subscription
+              .listSubscribers(name)
+              .map(subscribers => name -> RepositoryStatus(version, subscribers))
         }
+        .map(_.toMap)
+
+    private def latestRepositoryVersions(repos: Set[RepositoryName]): IO[PagerError, Map[RepositoryName, Option[Version]]] =
+      ZIO
+        .traverse(repos) { repositoryName =>
+          gitHubClient
+            .releases(repositoryName)
+            .map(releases => repositoryName -> Try(releases.maxBy(_.published_at).name).toOption)
+        }
+        .map(_.toMap)
+
+    private def newVersions(
+      latestKnownReleases: Map[RepositoryName, Option[Version]],
+      latestReleases: Map[RepositoryName, Option[Version]]
+    ): Map[RepositoryName, Version] =
+      latestKnownReleases.flatMap {
+        case (name, latestKnownVersion) =>
+          latestReleases
+            .get(name)
+            .collect { case Some(latestVersion) if !latestKnownVersion.contains(latestVersion) => name -> latestVersion }
+      }
+
+    private def broadcastUpdates(repos: Map[RepositoryName, RepositoryStatus]): Task[Unit] =
+      ZIO
+        .foreach(repos)(telegramClient.broadcastNewVersion _ tupled)
         .unit
   }
 }
