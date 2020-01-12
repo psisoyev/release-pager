51cb9dce1c18af269bd47368249e816db27a7c8b
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-01-12 21:04:56 +0100

    add convenience methods

--- service/src/test/scala/io/pager/package.scala
+++ service/src/test/scala/io/pager/package.scala
@@ -1,8 +1,12 @@
 package io
 
+import zio.{ Ref, UIO }
 import zio.random.Random
 import zio.test.{ Sized, ZSpec }
 
 package object pager {
   type TestScenarios = List[ZSpec[Random with Sized, Throwable, String, Unit]]
+
+  def mkMap[K, V](map: Map[K, V]): UIO[Ref[Map[K, V]]] = Ref.make(map)
+  def emptyMap[K, V]: UIO[Ref[Map[K, V]]]              = mkMap(Map.empty[K, V])
 }
