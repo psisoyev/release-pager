67e91d90b9eea83601d3ce392724275faad1dcdd
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-11-27 15:54:17 +0100

    Update zio-interop-cats to 2.2.0.1 (#21)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
 
 object Version {
   val zio = "1.0.3"
-  val zioCats = "2.1.4.1"
+  val zioCats = "2.2.0.1"
   val slf4j = "1.7.30"
   val fs2Core = "2.4.6"
   val kindProjector = "0.10.3"
