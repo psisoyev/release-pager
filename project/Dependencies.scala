8cd83272d20332ddd25e5e296f00a8921d0eed6f
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-04-24 01:05:26 +0000

    Update fs2-core to 2.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Version {
   val zio = "1.0.6"
   val zioCats = "2.4.0.0"
   val slf4j = "1.7.30"
-  val fs2Core = "2.5.4"
+  val fs2Core = "2.5.5"
   val kindProjector = "0.10.3"
   val canoe = "0.5.1"
   val circe = "0.13.0"
