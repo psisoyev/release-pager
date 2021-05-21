20dad0e5bf48c23acb01ceddc06731a7b941b6d4
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-05-21 15:29:18 +0200

    Update flyway-core to 7.9.1 (#89)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Version {
   val canoe = "0.5.1"
   val circe = "0.13.0"
   val doobie = "0.13.2"
-  val flyway = "7.8.2"
+  val flyway = "7.9.1"
   val h2 = "1.4.200"
   val pureconfig = "0.15.0"
 }
