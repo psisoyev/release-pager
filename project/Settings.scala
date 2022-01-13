a9073f37b5d19641ba031fec8c42df4d65e350fc
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-01-13 10:57:59 +0100

    Update scala-library to 2.13.8 (#186)

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,10 +1,10 @@
 import Dependencies._
 import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
-import sbt.Keys.{scalacOptions, _}
+import sbt.Keys.{ scalacOptions, _ }
 import sbt._
 import sbt.util.Level
 import wartremover.WartRemover.autoImport.wartremoverErrors
-import wartremover.{Wart, Warts}
+import wartremover.{ Wart, Warts }
 
 object Settings {
   val warts = Warts.allBut(
@@ -16,15 +16,17 @@ object Settings {
     Wart.JavaSerializable,
     Wart.PublicInference,
     Wart.Serializable,
-    Wart.DefaultArguments)
+    Wart.DefaultArguments
+  )
 
-  val commonSettings = {
+  val commonSettings =
     Seq(
-      scalaVersion := "2.13.7",
+      scalaVersion := "2.13.8",
       scalacOptions := Seq(
         "-Ymacro-annotations",
         "-deprecation",
-        "-encoding", "utf-8",
+        "-encoding",
+        "utf-8",
         "-explaintypes",
         "-feature",
         "-unchecked",
@@ -40,12 +42,10 @@ object Settings {
       wartremoverErrors in (Compile, compile) ++= warts,
       wartremoverErrors in (Test, compile) ++= warts,
       testFrameworks := Seq(new TestFramework("zio.test.sbt.ZTestFramework")),
-
       cancelable in Global := true,
       fork in Global := true, // https://github.com/sbt/sbt/issues/2274
       resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     )
-  }
 
   val storageDependencies = List(zio, zioCats) ++ doobie
   val serviceDependencies = List(zioCats, zioMacros, zioTest, zioTestSbt, fs2Core, canoe, slf4j) ++ circe
