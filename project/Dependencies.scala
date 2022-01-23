6b5172dd9c520d22e82ca13e840c7783d939e0bf
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-01-23 22:10:50 +0100

    Update flyway-core to 8.4.2 (#190)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Version {
   val canoe      = "0.5.1"
   val circe      = "0.14.1"
   val doobie     = "0.13.4"
-  val flyway     = "8.4.1"
+  val flyway     = "8.4.2"
   val h2         = "2.0.206"
   val pureconfig = "0.17.1"
 }
