fdb9af4a144a0513973d2c7b92163c066782add2
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-04-12 23:03:19 +0200

    Remove duplicates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,5 @@ object Version {
   val doobie     = "0.13.4"
   val h2         = "2.1.212"
   val flyway     = "8.5.7"
-  val h2         = "2.1.212"
-  val flyway     = "8.5.5"
   val pureconfig = "0.17.1"
 }
