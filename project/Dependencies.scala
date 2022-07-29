231fe19d77b1c54bb4029145ce6d2113bf5b2287
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-07-29 02:12:57 +0000

    Update flyway-core to 9.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,6 +39,6 @@ object Version {
   val circe      = "0.14.1"
   val doobie     = "0.13.4"
   val h2         = "2.1.214"
-  val flyway     = "9.0.1"
+  val flyway     = "9.0.3"
   val pureconfig = "0.17.1"
 }
