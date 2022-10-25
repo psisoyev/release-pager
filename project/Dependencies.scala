431f218237c58dd8a56338ce2973d13fff7e89a9
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-10-25 02:32:52 +0000

    Update circe-generic-extras to 0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Version {
   val slf4j      = "2.0.3"
   val fs2Core    = "3.3.0"
   val canoe      = "0.6.0"
-  val circe      = "0.14.2"
+  val circe      = "0.14.3"
   val doobie     = "1.0.0-RC2"
   val h2         = "2.1.212"
   val flyway     = "8.5.8"
