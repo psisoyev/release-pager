61087eb4c386c278804275254b76695c3c982e59
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-12-13 01:38:50 +0000

    Update zio, zio-test, zio-test-sbt to 2.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
 }
 
 object Version {
-  val zio        = "2.0.2"
+  val zio        = "2.0.5"
   val zioCats    = "23.0.0.0"
   val slf4j      = "2.0.5"
   val fs2Core    = "3.4.0"
