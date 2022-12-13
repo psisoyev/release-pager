dea52d06c67a230f44143bca0d7c20227318cd69
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-12-13 01:38:48 +0000

    Revert commit(s) 99b3926

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
 }
 
 object Version {
-  val zio        = "2.0.5"
+  val zio        = "2.0.2"
   val zioCats    = "3.3.0"
   val slf4j      = "2.0.3"
   val fs2Core    = "3.4.0"
