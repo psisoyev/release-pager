94dee7d0db380d64259860acc5236c0fb60ca2c7
Pavels Sisojevs <p.sisoyev@gmail.com>
2023-06-21 01:27:39 +0000

    Update doobie-core, doobie-h2, ... to 1.0.0-RC4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Version {
   val fs2Core    = "3.6.1"
   val canoe      = "0.6.0"
   val circe      = "0.14.3"
-  val doobie     = "1.0.0-RC2"
+  val doobie     = "1.0.0-RC4"
   val h2         = "2.1.212"
   val flyway     = "8.5.8"
   val pureconfig = "0.17.3"
