6b1c16983c3e575749966d3e2914cd386b727a08
Pavels Sisojevs <p.sisoyev@gmail.com>
2024-03-25 01:10:38 +0000

    Update fs2-core to 3.10.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
 object Version {
   val zio        = "2.0.2"
   val zioCats    = "23.0.0.1"
-  val fs2Core    = "3.9.4"
+  val fs2Core    = "3.10.1"
   val slf4j      = "2.0.12"
   val canoe      = "0.6.0"
   val circe      = "0.14.3"
